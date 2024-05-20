c82631bdc659e674edb6ffb4a4994ed72353f1c0
hibana2077 <hibana2077@gmail.com>
2024-05-20 17:18:55 +0800

    chore: Update main.py to print accuracy with 4 decimal places

--- train/main.py
+++ train/main.py
@@ -2,7 +2,7 @@
 Author: hibana2077 hibana2077@gmail.com
 Date: 2024-05-20 15:37:01
 LastEditors: hibana2077 hibana2077@gmail.com
-LastEditTime: 2024-05-20 17:08:05
+LastEditTime: 2024-05-20 17:18:51
 FilePath: \Muffin-vs-chihuahua\train\main.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE/
 '''
@@ -122,7 +122,7 @@ with torch.no_grad():
         total += labels.size(0)
         correct += (predicted == labels).sum().item()
 
-print(f"Accuracy: {100*correct/total}%")
+print(f"Accuracy: {100*correct/total:.4f}%")
 
 # random show image
 
